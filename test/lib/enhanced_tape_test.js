012e89be62e3c8b900193a125f3d1a03995e454d
Alexey D <ord.alwo@gmail.com>
2020-08-11 11:56:05 +0300

    Add beforeEach()

--- test/lib/enhanced_tape_test.js
+++ test/lib/enhanced_tape_test.js
@@ -1,7 +1,22 @@
 import test from "enhanced_tape";
 import assertTestFails from "test/support/assert_test_fails";
 
-test("tape", function(t) {  
+let beforeEachCalledTimes = 0;
+test.beforeEach(() => {
+  beforeEachCalledTimes += 1;
+});
+
+test("tape", function(t) {
+  t.test("beforeEach()", function(t) {
+    t.equal(beforeEachCalledTimes, 2);
+
+    t.test("runs function before each test", function(t) {
+      t.equal(beforeEachCalledTimes, 3);
+  
+      t.end();
+    });
+  });
+
   t.test("error handling", function(t) {
     t.test("catches errors", function(t) {
       assertTestFails(t, () => { throw new Error("foo"); }, /foo/);
@@ -14,20 +29,20 @@ test("tape", function(t) {
     
       t.end();
     });
+  });
 
-    t.test("extends test name", function(t) {
-      t.match(t.name, /tape error handling/);
-    
-      t.end();
-    });
+  t.test("extends test name", function(t) {
+    t.match(t.name, /tape/);
+  
+    t.end();
+  });
 
-    t.test("automatically ends tests", function(t) {
-      t.pass();
-    });
+  t.test("automatically ends tests", function(t) {
+    t.pass();
+  });
 
-    t.test("automatically ends async tests", async function(t) {
-      await new Promise(resolve => resolve());
-      t.pass();
-    });
+  t.test("automatically ends async tests", async function(t) {
+    await new Promise(resolve => resolve());
+    t.pass();
   });
 });
